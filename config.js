e7adcc98a39ea4178252ac9b4c2eb6facd350004
Julian Camillus Mensah <90489359+juliancamillusmensah@users.noreply.github.com>
2024-02-18 01:28:50 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['0242430892', 'MADARA', true],
+  ['0501889982', 'JULIAN', true], 
   [''] 
 ] //Number of owners
 
@@ -50,16 +50,14 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'MADARA UCHIHA'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'MADARA┃ᴮᴼᵀ' 
+global.author = '@Julian' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.igfg = '▢ Follow on https://www.instagram.com/juliancamillus.mensah/' 
+global.fgyt = 'https://github.com/juliancamillusmensah'
+global.fgpyp = 'https://www.youtube.com/@julian17media'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
